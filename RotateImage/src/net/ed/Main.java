3ea0c64f64a599ee7e5fd01d9d06c3e19923f1ad
Edward Dupre <edwd42@gmail.com>
2017-12-24 11:29:30 -0500

    rotating array ccw, need to reverse direction to cw

--- RotateImage/src/net/ed/Main.java
+++ RotateImage/src/net/ed/Main.java
@@ -23,7 +23,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+//        int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
 
         System.out.println("CodeFights.com RotateImage Challenge");
 
@@ -33,6 +34,7 @@ public class Main {
     public static String rotateImage(int[][] arr) {
         int aLength = arr.length;
 
+        // print the array passed in
         for(int r = 0;  r < aLength; r++) {
             for (int c = 0; c < aLength; c++) {
                 System.out.print(arr[r][c] + " ");
@@ -50,29 +52,62 @@ public class Main {
         int r2 = 0;
         int c2 = 0;
 
+        System.out.println();
+
+        // print the array [r][c] grid
         for(r = 0;  r < aLength; r++) {
-            arr2[r2] = arr[r];
             for (c = 0; c < aLength; c++) {
-                arr2[c2] = arr[c];
-                System.out.println(r + " " + c);
+                System.out.print("[" + r + "][" + c + "]");
+                if(c < aLength - 1)
+                    System.out.print(" ");
             }
             System.out.println();
         }
 
-        for(c2 = aLength; c2 >= 0; c2--) {
+        // rotate the array cw
+        for(r = aLength - 1; r > 0; r--) {
+
+            for (c = aLength - 1; c > 0; c--) {
+                arr2[c2] = arr[r];
+                arr2[r2] = arr[c];
+            }
+        }
+
+        // rotates the array ccw
+//        for(r = 0;  r < aLength; r++) {
+//            arr2[c2] = arr[r];
+//            for (c = 0; c < aLength; c++) {
+//                arr2[r2] = arr[c];
+//            }
+//        }
+
+        System.out.println();
+
+        // print the rotated [r][c] grid
+        for(c2 = aLength - 1; c2 >= 0; c2--) {
             for (r2 = 0; r2 < aLength; r2++) {
-                System.out.println(r2 + " " + c2);
+                System.out.print("[" + r2 + "][" + c2 + "]");
+                if(c2 < aLength)
+                    System.out.print(" ");
             }
             System.out.println();
         }
 
+        System.out.println();
+
+        // print the array elements after rotating
         for(r = 0;  r < aLength; r++) {
             for (c = 0; c < aLength; c++) {
                 System.out.print(arr2[r][c] + " ");
             }
             System.out.println();
         }
-        return("\n" + arr[0] + "\n" + arr[1] + "\n" + arr[2] + "\n" + arr[3]);
+
+        // 4x4 array
+//        return("\n" + arr[0] + "\n" + arr[1] + "\n" + arr[2] + "\n" + arr[3]);
+
+        // 3x3 array
+        return("\n" + arr[0] + "\n" + arr[1] + "\n" + arr[2]);
     }
 
 }
