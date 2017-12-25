c890e6c1e1c8ca4572b0ac70f9d89e6efb00d056
Edward Dupre <edwd42@gmail.com>
2017-12-25 11:50:38 -0500

    add for loops for temp array inside of original for loops

--- RotateImage/src/net/ed/Main.java
+++ RotateImage/src/net/ed/Main.java
@@ -35,6 +35,7 @@ public class Main {
         int aLength = arr.length;
 
         // print the array passed in
+        System.out.println("original array");
         for(int r = 0;  r < aLength; r++) {
             for (int c = 0; c < aLength; c++) {
                 System.out.print(arr[r][c] + " ");
@@ -54,7 +55,8 @@ public class Main {
 
         System.out.println();
 
-        // print the array [r][c] grid
+        // print the grid [r][c] before rotating
+        System.out.println("print grid before rotating");
         for(r = 0;  r < aLength; r++) {
             for (c = 0; c < aLength; c++) {
                 System.out.print("[" + r + "][" + c + "]");
@@ -63,45 +65,72 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println();
 
-        // rotate the array cw
-        for(r = aLength - 1; r > 0; r--) {
-
-            for (c = aLength - 1; c > 0; c--) {
-                arr2[c2] = arr[r];
-                arr2[r2] = arr[c];
+        // rotate the grid cw
+        for(c2 = aLength - 1; c2 >= 0; c2--) {
+            for (r2 = 0;  r2 < aLength; r2++) {
+                for (c = 0; c < aLength; c++) {
+                    for (r =0; r < aLength; r++) {
+                        arr2[r][c] = arr[r2][r2];
+//                        arr2[r][c] = arr[r2][c2];
+                    }
+                }
             }
         }
 
-        // rotates the array ccw
-//        for(r = 0;  r < aLength; r++) {
-//            arr2[c2] = arr[r];
+
+        // rotate a second time
+//        for (r = 0;  r < aLength; r++) {
 //            for (c = 0; c < aLength; c++) {
-//                arr2[r2] = arr[c];
+////            for(c = aLength - 1; c >= 0; c--) {
+//                arr2[r][c] = arr2[c][r];
 //            }
 //        }
 
-        System.out.println();
-
-        // print the rotated [r][c] grid
-        for(c2 = aLength - 1; c2 >= 0; c2--) {
-            for (r2 = 0; r2 < aLength; r2++) {
-                System.out.print("[" + r2 + "][" + c2 + "]");
-                if(c2 < aLength)
-                    System.out.print(" ");
-            }
-            System.out.println();
-        }
-
-        System.out.println();
-
-        // print the array elements after rotating
-        for(r = 0;  r < aLength; r++) {
-            for (c = 0; c < aLength; c++) {
+        for( r = 0;  r < aLength; r++) {
+            for ( c = 0; c < aLength; c++) {
                 System.out.print(arr2[r][c] + " ");
             }
             System.out.println();
         }
+
+        System.out.println();
+
+        // print the grid [r][c] after rotating
+//        System.out.println("print grid after rotating");
+//        for(r = 0;  r < aLength; r++) {
+//            for (c = 0; c < aLength; c++) {
+//                System.out.print("[" + r + "][" + c + "]");
+//                if(c < aLength - 1)
+//                    System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+
+
+        // print the rotated [r][c] grid
+//        System.out.println("print grid after rotating");
+//        for(c2 = aLength - 1; c2 >= 0; c2--) {
+//            for (r2 = 0; r2 < aLength; r2++) {
+//                System.out.print("[" + r2 + "][" + c2 + "]");
+//                if(c2 < aLength)
+//                    System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
+
+        System.out.println();
+
+        // print the array elements after rotating
+//        System.out.println("array after rotating");
+//        for(r = 0;  r < aLength; r++) {
+//            for (c = 0; c < aLength; c++) {
+//                System.out.print(arr2[r][c] + " ");
+//            }
+//            System.out.println();
+//        }
 
         // 4x4 array
 //        return("\n" + arr[0] + "\n" + arr[1] + "\n" + arr[2] + "\n" + arr[3]);
