6a3b4fda66d025bcb70bb289519eb2b29feff910
Edward Dupre <edwd42@gmail.com>
2017-12-25 13:18:38 -0500

    assign c to r --> r = c then c decrements from aLength-1 to 0

--- RotateImage/src/net/ed/Main.java
+++ RotateImage/src/net/ed/Main.java
@@ -67,17 +67,30 @@ public class Main {
         }
         System.out.println();
 
-        // rotate the grid cw
-        for(c2 = aLength - 1; c2 >= 0; c2--) {
-            for (r2 = 0;  r2 < aLength; r2++) {
-                for (c = 0; c < aLength; c++) {
-                    for (r =0; r < aLength; r++) {
-                        arr2[r][c] = arr[r2][r2];
-//                        arr2[r][c] = arr[r2][c2];
-                    }
+
+        // assign c to r --> r = c
+        // then c decrements from aLength-1 to 0
+        for (int i = 0; i < aLength; i++) {
+            for (c = aLength - 1; c >= 0; c--) {
+                for (r =0; r < aLength; r++) {
+
+                    arr2[r][r] = arr[r][c];
+                    // arr2[r][c] = arr[r2][c2];
                 }
             }
         }
+
+        // rotate the grid cw
+//        for(c2 = aLength - 1; c2 >= 0; c2--) {
+//            for (r2 = aLength - 1; r2 >= 0; r2--) {
+//                for (c = 0; c < aLength; c++) {
+//                    for (r =0; r < aLength; r++) {
+//                        arr2[c][r] = arr[r2][c2];
+////                        arr2[r][c] = arr[r2][c2];
+//                    }
+//                }
+//            }
+//        }
 
 
         // rotate a second time
